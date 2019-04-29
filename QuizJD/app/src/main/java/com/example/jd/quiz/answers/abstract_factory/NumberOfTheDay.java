25a804053cd0327ed10cda97af3e28296fd9a924
GodDAN <scoliztur@gmail.com>
2019-04-29 23:52:36 +0300

    quiz

---
+++ QuizJD/app/src/main/java/com/example/jd/quiz/answers/abstract_factory/NumberOfTheDay.java
@@ -0,0 +1,169 @@
+package com.example.jd.quiz.answers.abstract_factory;
+
+public class NumberOfTheDay {
+
+    private int day1, day2, day3, day4, day5,
+            day6, day7, day8, day9, day10,
+            day11, day12, day13, day14, day15,
+            day16, day17, day18, day19, day20,
+            day21, day22, day23, day24, day25,
+            day26, day27, day28, day29, day30;
+
+    public NumberOfTheDay(int day1, int day2, int day3, int day4, int day5,
+                          int day6, int day7, int day8, int day9, int day10,
+                          int day11, int day12, int day13, int day14, int day15,
+                          int day16, int day17, int day18, int day19, int day20,
+                          int day21, int day22, int day23, int day24, int day25,
+                          int day26, int day27, int day28, int day29, int day30) {
+        this.day1 = day1;
+        this.day2 = day2;
+        this.day3 = day3;
+        this.day4 = day4;
+        this.day5 = day5;
+        this.day6 = day6;
+        this.day7 = day7;
+        this.day8 = day8;
+        this.day9 = day9;
+        this.day10 = day10;
+        this.day11 = day11;
+        this.day12 = day12;
+        this.day13 = day13;
+        this.day14 = day14;
+        this.day15 = day15;
+        this.day16 = day16;
+        this.day17 = day17;
+        this.day18 = day18;
+        this.day19 = day19;
+        this.day20 = day20;
+        this.day21 = day21;
+        this.day22 = day22;
+        this.day23 = day23;
+        this.day24 = day24;
+        this.day25 = day25;
+        this.day26 = day26;
+        this.day27 = day27;
+        this.day28 = day28;
+        this.day29 = day29;
+        this.day30 = day30;
+    }
+
+    public int getDay1() {
+        return day1;
+    }
+
+    public int getDay2() {
+        return day2;
+    }
+
+    public int getDay3() {
+        return day3;
+    }
+
+    public int getDay4() {
+        return day4;
+    }
+
+    public int getDay5() {
+        return day5;
+    }
+
+    public int getDay6() {
+        return day6;
+    }
+
+    public int getDay7() {
+        return day7;
+    }
+
+    public int getDay8() {
+        return day8;
+    }
+
+    public int getDay9() {
+        return day9;
+    }
+
+    public int getDay10() {
+        return day10;
+    }
+
+    public int getDay11() {
+        return day11;
+    }
+
+    public int getDay12() {
+        return day12;
+    }
+
+    public int getDay13() {
+        return day13;
+    }
+
+    public int getDay14() {
+        return day14;
+    }
+
+    public int getDay15() {
+        return day15;
+    }
+
+    public int getDay16() {
+        return day16;
+    }
+
+    public int getDay17() {
+        return day17;
+    }
+
+    public int getDay18() {
+        return day18;
+    }
+
+    public int getDay19() {
+        return day19;
+    }
+
+    public int getDay20() {
+        return day20;
+    }
+
+    public int getDay21() {
+        return day21;
+    }
+
+    public int getDay22() {
+        return day22;
+    }
+
+    public int getDay23() {
+        return day23;
+    }
+
+    public int getDay24() {
+        return day24;
+    }
+
+    public int getDay25() {
+        return day25;
+    }
+
+    public int getDay26() {
+        return day26;
+    }
+
+    public int getDay27() {
+        return day27;
+    }
+
+    public int getDay28() {
+        return day28;
+    }
+
+    public int getDay29() {
+        return day29;
+    }
+
+    public int getDay30() {
+        return day30;
+    }
+}
---
+++ QuizJD/app/src/main/java/com/example/jd/quiz/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jd.quiz.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jd.quiz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
